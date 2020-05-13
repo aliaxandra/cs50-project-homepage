ab80b13b1e5cbab247c2625f51964784553e357f
Alexandra Sergeychik <aliaxandra@gmail.com>
2020-05-13 17:01:04 +0300

    refactor: status js

--- homepage/script.js
+++ homepage/script.js
@@ -1,40 +1,43 @@
+setDashboardStatus();
+
 // About alert
 document.querySelector("#about").onclick = function () {
     alert("About Text");
 }
 
-// Status change
-let statusResult = document.querySelector('#statusResult');
-let statusNew = document.querySelector('#statusNew');
-let statusSubmit = document.querySelector('#statusSubmit');
-
-if (!localStorage.getItem('statusNew')) {
-    populateStorage();
-} 
-else 
-{
-    setStatus();
-}
-
-function populateStorage()
-{
-    localStorage.setItem('statusKey', document.querySelector('#statusNew').value);
-    setStatus();
-}
-
-function setStatus()
-{
-    var currentStatus = localStorage.getItem('statusKey');
-    statusResult.innerHTML = currentStatus;
-}
-
-statusSubmit.onclick = populateStorage;
-
 // Today date
 let today = new Date();
 document.querySelector("#today").innerHTML = today.toDateString();
 
+// setDashboardStatus - Status change
+function setDashboardStatus() {
+    
+    let statusResult = document.querySelector('#statusResult');
+    let statusNew = document.querySelector('#statusNew');
+    let statusSubmit = document.querySelector('#statusSubmit');
+    
+    if (!localStorage.getItem('statusNew')) {
+        populateStorage();
+    }
+    else {
+        setStatus();
+    }
+    
+    function populateStorage() {
+        localStorage.setItem('statusKey', document.querySelector('#statusNew').value);
+        setStatus();
+    }
+    
+    function setStatus() {
+        var currentStatus = localStorage.getItem('statusKey');
+        statusResult.innerHTML = currentStatus;
+    }
+    
+    statusSubmit.onclick = populateStorage;
+}
+
 // Done button
+
 document.querySelector("#done").onclick = function() {
     document.querySelector("#encouraging").innerHTML = 'Good job!';
 }
