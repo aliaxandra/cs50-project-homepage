941ef4d488387d57089f806c6244e3ef9e128469
Alexandra Sergeychik <aliaxandra@gmail.com>
2020-05-20 17:47:43 +0300

    fix: work on Done button

--- homepage/script.js
+++ homepage/script.js
@@ -1,4 +1,6 @@
-editStatus()
+editStatus();
+editDate();
+setTodayDate();
 
 // About alert
 document.querySelector("#about").onclick = function () {
@@ -6,47 +8,55 @@ document.querySelector("#about").onclick = function () {
 }
 
 // Today date
-let today = new Date();
-document.querySelector("#today").innerHTML = today.toDateString();
+function setTodayDate()
+{
+    let today = new Date();
+    let todayDate = today.toDateString();
+    return todayDate;
+}
+
+// Set today date
+document.querySelector("#today").innerHTML = setTodayDate();
 
 // Status button
 function editStatus() {
     
     let statusResult = document.querySelector('#statusResult');
     let statusSubmit = document.querySelector('#statusSubmit');
-    let statusNew = document.querySelector('#statusNew');
-    let editButton = document.querySelector('#editButton');
-    let editForm = document.querySelector('#editForm');
 
     // Open Edit form on Edit button
+    let editButton = document.querySelector('#editButton');
+    let editForm = document.querySelector('#editForm');
     editButton.onclick = function () {
         editForm.classList.remove('hide');
         editButton.classList.add('hide');
     }
     
     // Check whether we have stored string in local storage
-    if (!localStorage.getItem('statusNew')) {
+    if (!localStorage.getItem('statusResult')) {
         populateStorage();
     }
     else {
-        setStatus();
+        setStorage();
     }
     
     // Store the string and show new status
-    function populateStorage() {
+    function populateStorage() 
+    {
         // Store key/value pair
-        localStorage.setItem('statusKey', statusNew.value);
-        setStatus();
+        localStorage.setItem('statusResult', document.querySelector('#statusNew').value);
+        setStorage();
+
         // Hide Edit form, show Edit button
         editForm.classList.add('hide');
         editButton.classList.remove('hide');
     }
     
     // Get the string from local storage and show in the Status 
-    function setStatus() {
-
+    function setStorage() 
+    {
         // Get value by key
-        let currentStatus = localStorage.getItem('statusKey');
+        let currentStatus = localStorage.getItem('statusResult');
 
         // Check is string is empty or not
         if (currentStatus == ""){
@@ -56,15 +66,49 @@ function editStatus() {
         }
         else {
             // If string isn't empty - show it in Status
-            statusResult.innerHTML = currentStatus;   
+            document.querySelector('#statusResult').innerHTML = currentStatus;   
+            statusResult.innerHTML = currentStatus;  
         }
     }
     
     // Store new string on click Submit button
-    statusSubmit.onclick = populateStorage;
+    statusSubmit.onclick = populateStorage();
 }
 
 // Done button
-document.querySelector("#done").onclick = function() {
-    document.querySelector("#encouraging").innerHTML = today.toDateString();
+
+function editDate() {
+
+    let doneResult = document.querySelector('#doneResult');
+    let doneButton = document.querySelector('#doneButton');
+    
+
+    // Check whether we have stored string in local storage
+    if (!localStorage.getItem('dateNew')) {
+        populateStorage();
+    }
+    else {
+        setStorage();
+    }
+
+    // Store the string and show new date
+    function populateStorage()
+    {
+        let dateNew = setTodayDate();
+        // Store key/value pair
+        localStorage.setItem('dateKey', dateNew);
+        setStorage();
+    }
+
+    // Get the string from local storage and show in the date done
+    function setStorage()
+    {
+        // Get value by key
+        let currentDate = localStorage.getItem('dateKey');
+        doneResult.innerHTML = currentDate;
+        
+    }
+
+    doneButton.onclick = populateStorage();
+
 }
